853d826d95954136f81eabb62cc07c5bbb47408a
lildesert <julien.bistoquet@gmail.com>
2017-01-02 17:26:51 -0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: "AIzaSyB0wbAt5f0Mleil0nyGVkSQM3t57nzicK0",
+  authDomain: "cookie-b5fe6.firebaseapp.com",
+  databaseURL: "https://cookie-b5fe6.firebaseio.com",
+  storageBucket: "cookie-b5fe6.appspot.com",
+  messagingSenderId: "923182330691"
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     FormsModule,
     HttpModule
   ],
   providers: [],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
